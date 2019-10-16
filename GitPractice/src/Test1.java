fea5ac4b1514384f2b0eeebecd2919f9f2b589b0
Nick TestGit <someone@gmail.com>
2019-10-15 22:38:48 -0400

    Removed a line of code.

--- GitPractice/src/Test1.java
+++ GitPractice/src/Test1.java
@@ -4,7 +4,8 @@ public class Test1 {
 	public static void main(String[] args) {
 		System.out.println("It's a test class.");
 		System.out.println("It's another test line.");
-		System.out.println("One more test line. It's a third line.");
+		//this code is not needed.
+		//System.out.println("One more test line. It's a third line.");
 
 	}
 
